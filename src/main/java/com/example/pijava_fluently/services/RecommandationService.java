4f75957ac50098cc2c607aaf5f3ddd3984eaf680
Yosr Ben Hamouda <yosr.benhamouda@esprit.tn>
2026-04-26 19:52:57 +0100

    Fix missing OutputStream import in RecommandationService

--- src/main/java/com/example/pijava_fluently/services/RecommandationService.java
+++ src/main/java/com/example/pijava_fluently/services/RecommandationService.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
